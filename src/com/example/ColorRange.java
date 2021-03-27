9be2aa3658af9dd1cfb064974f32f06c069b660d
mariamnazri <78435361+mariamnazri@users.noreply.github.com>
2021-03-26 22:11:42 -0400

    CheckColor

--- src/com/example/ColorRange.java
+++ src/com/example/ColorRange.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example;
 
 import java.util.Scanner;
@@ -10,6 +5,32 @@ import java.util.Scanner;
 public class ColorRange {
 
     public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
         
+        System.out.println("Enter a colour code: ");
+        double wavelength = input.nextDouble();
+        
+
+        if(wavelength >= 380 && wavelength < 450){
+            System.out.println("The colour is Violet");
+        }
+        else if(wavelength >= 450 && wavelength < 495){
+            System.out.println("The colour is Blue");
+        }
+        else if(wavelength >= 495 && wavelength < 570){
+            System.out.println("The colour is Green");
+        }
+        else if(wavelength >= 570 && wavelength < 590){
+            System.out.println("The colour is Yellow");
+        }
+        else if(wavelength >= 590 && wavelength < 620){
+            System.out.println("The colour is Orange");
+        }
+        else if(wavelength >= 620 && wavelength < 750){
+            System.out.println("The colour is Red");
+        }
+        else{
+            System.out.println("The entered wavelength is not a part of the visible spectrum");
         }
     }
+}
